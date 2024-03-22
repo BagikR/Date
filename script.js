483ec80958051192fe5547ec01bda7b79d98e39e
BagikR <99125716+BagikR@users.noreply.github.com>
2024-03-22 15:05:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+document.getElementById('digitsForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const digits = document.getElementById('digits').value; // Отримати рядок цифр
+    const birthDigits = digits.substr(0, 5); // Перші 5 цифр - дата народження
+    const genderDigit = digits.charAt(8); // 9 цифра - позначення статі
+
+    const birthDate = formatDate(birthDigits);
+    const gender = determineGender(parseInt(genderDigit));
+
+    document.getElementById('result').innerHTML = `
+        <p>Дата народження: ${birthDate}</p>
+        <p>Стать: ${gender}</p>
+    `;
+});
+
+function formatDate(digits) {
+    const birthDate = new Date(1899, 11, 31);
+    const days = parseInt(digits);
+    birthDate.setDate(birthDate.getDate() + days);
+    const day = String(birthDate.getDate()).padStart(2, '0');
+    const month = String(birthDate.getMonth() + 1).padStart(2, '0');
+    const year = birthDate.getFullYear();
+
+    return `${day}.${month}.${year}`;
+}
+
+function determineGender(digit) {
+    return digit % 2 === 0 ? 'жінка' : 'чоловік';
+}
